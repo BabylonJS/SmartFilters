f64df6ea4789afbcedbf9e68c643487f005a3dfe
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2024-07-03 11:36:37 -0700

    Added alpha prerelease tag support any many more tests

--- packages/core/test/unit/determineVersion.test.js
+++ packages/core/test/unit/determineVersion.test.js
@@ -1,37 +1,131 @@
-import { determineVersion } from "../../tools/determineVersion.js";
+import { determineVersion, removePrereleaseFlags, getNpmVersion } from "../../tools/determineVersion.js";
 
 describe("versionUp", () => {
-    describe("when NPM has no version yet", () => {
-        let npmVersion = null;
+    beforeAll(() => {
+        global.console = {
+            log: jest.fn(),
+            warn: jest.fn(),
+            error: jest.fn(),
+        };
+    });
 
-        it("should use the package.json version", () => {
-            const packageJsonVersion = "1.0.0";
-            expect(determineVersion(npmVersion, packageJsonVersion)).toBe(packageJsonVersion);
+    describe("determineVersion", () => {
+        describe("when NPM has no version yet", () => {
+            let npmVersion = null;
+
+            it("should use the package.json version", () => {
+                const packageJsonVersion = "1.0.0";
+                expect(determineVersion(npmVersion, packageJsonVersion)).toBe(packageJsonVersion);
+            });
+        });
+
+        describe("when NPM has a version", () => {
+            let npmVersion = "2.3.4";
+
+            describe("when package.json has the same major and minor version", () => {
+                it("should use the npm version and increment the patch version", () => {
+                    const packageJsonVersion = "2.3.0";
+                    expect(determineVersion(npmVersion, packageJsonVersion)).toBe("2.3.5");
+                });
+            });
+
+            describe("when package.json has the same major and a different minor version", () => {
+                it("should use the npm version and increment the patch version", () => {
+                    const packageJsonVersion = "2.4.1";
+                    expect(determineVersion(npmVersion, packageJsonVersion)).toBe("2.4.1");
+                });
+            });
+
+            describe("when package.json has a different major and minor version", () => {
+                it("should use the npm version and increment the patch version", () => {
+                    const packageJsonVersion = "9.9.6";
+                    expect(determineVersion(npmVersion, packageJsonVersion)).toBe("9.9.6");
+                });
+            });
+
+            describe("when NPM version has a prerelease flag", () => {
+                let npmVersion = "0.1.0-alpha";
+
+                describe("when package.json has the same major and minor version", () => {
+                    it("should use the npm version and increment the patch version, removing the prerelease flag", () => {
+                        // NOTE: versionUp.js may decide to put the pre-release flag back
+                        const packageJsonVersion = "0.1.0";
+                        expect(determineVersion(npmVersion, packageJsonVersion)).toBe("0.1.1");
+                    });
+                });
+            });
+        });
+
+        it.each([
+            {
+                npmVersion: "0.1.0-alpha",
+                packageJsonVersion: "0.1.0",
+                alpha: true,
+                expectedVersion: "0.1.1-alpha",
+            },
+            {
+                npmVersion: "0.1.0-alpha",
+                packageJsonVersion: "0.2.0",
+                alpha: true,
+                expectedVersion: "0.2.0-alpha",
+            },
+            {
+                npmVersion: "0.2.3-alpha",
+                packageJsonVersion: "0.2.0",
+                alpha: false,
+                expectedVersion: "0.2.4",
+            },
+            {
+                npmVersion: "1.2.3",
+                packageJsonVersion: "1.2.0",
+                alpha: false,
+                expectedVersion: "1.2.4",
+            },
+            {
+                npmVersion: "1.2.3",
+                packageJsonVersion: "1.2.1000",
+                alpha: false,
+                expectedVersion: "1.2.4",
+            },
+            {
+                npmVersion: "1.2.3",
+                packageJsonVersion: "1.3.0",
+                alpha: false,
+                expectedVersion: "1.3.0",
+            },
+            {
+                npmVersion: "1.2.3",
+                packageJsonVersion: "2.0.0",
+                alpha: false,
+                expectedVersion: "2.0.0",
+            },
+        ])(
+            "when NPM version is $npmVersion and package json version is $packageJsonVersion and alpha is @alpha, it should return $expectedVersion",
+            ({ npmVersion, packageJsonVersion, alpha, expectedVersion }) => {
+                expect(determineVersion(npmVersion, packageJsonVersion, alpha)).toBe(expectedVersion);
+            }
+        );
+    });
+
+    describe("removePrereleaseFlags", () => {
+        it("should remove the prerelease flag", () => {
+            expect(removePrereleaseFlags("0.1.0-alpha")).toBe("0.1.0");
         });
     });
 
-    describe("when NPM has a version", () => {
-        let npmVersion = "2.3.4";
-
-        describe("when package.json has the same major and minor version", () => {
-            it("should use the npm version and increment the patch version", () => {
-                const packageJsonVersion = "2.3.0";
-                expect(determineVersion(npmVersion, packageJsonVersion)).toBe("2.3.5");
-            });
+    describe("getNpmVersion", () => {
+        it("should return the version if there is no error", () => {
+            expect(getNpmVersion(null, "1.2.3")).toBe("1.2.3");
         });
 
-        describe("when package.json has the same major and a different minor version", () => {
-            it("should use the npm version and increment the patch version", () => {
-                const packageJsonVersion = "2.4.1";
-                expect(determineVersion(npmVersion, packageJsonVersion)).toBe("2.4.1");
-            });
+        it("should return null if there is an E404 error", () => {
+            const err = new Error("E404");
+            expect(getNpmVersion(err, null)).toBeNull();
         });
 
-        describe("when package.json has a different major and minor version", () => {
-            it("should use the npm version and increment the patch version", () => {
-                const packageJsonVersion = "9.9.6";
-                expect(determineVersion(npmVersion, packageJsonVersion)).toBe("9.9.6");
-            });
+        it("should throw an error if there is a non-404 error", () => {
+            const err = new Error("Some other error");
+            expect(() => getNpmVersion(err, null)).toThrow(err);
         });
     });
 });
--- packages/core/tools/versionUp.js
+++ packages/core/tools/versionUp.js
@@ -1,27 +1,27 @@
 import * as fs from "fs";
 import { exec } from "child_process";
-import { determineVersion } from "./determineVersion.js";
+import { determineVersion, getNpmVersion } from "./determineVersion.js";
 
 exec("npm view @babylonjs/smart-filters dist-tags.preview", (err, stdout) => {
-    let npmVersion = null;
-    if (err?.message && err.message.indexOf("E404") !== -1) {
-        console.warn("NPM registry does not have a preview version.");
-    } else if (err) {
-        console.error(err);
-        throw err;
-    } else {
-        npmVersion = stdout;
-    }
+    const alpha = true;
 
-    console.log("Current NPM Registry version:", npmVersion);
     const packageText = fs.readFileSync("package.json");
     const packageJSON = JSON.parse(packageText);
+
+    let npmVersion = getNpmVersion(err, stdout);
+
+    console.log("Current NPM Registry version:", npmVersion);
     console.log("Current package.json version:", packageJSON.version);
 
-    let versionToUse = determineVersion(npmVersion, packageJSON.version);
+    let versionToUse = determineVersion(npmVersion, packageJSON.version, alpha);
+
     console.log("Version to use:", versionToUse);
 
-    packageJSON.version = versionToUse;
-    //fs.writeFileSync("package.json", JSON.stringify(packageJSON, null, 4));
-    console.log("Version updated in package.json");
+    if (packageJSON.version !== versionToUse) {
+        packageJSON.version = versionToUse;
+        fs.writeFileSync("package.json", JSON.stringify(packageJSON, null, 4));
+        console.log("Version updated in package.json");
+    } else {
+        console.log("No need to update package.json");
+    }
 });
