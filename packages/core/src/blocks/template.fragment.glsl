021e666c378473f110ced4a675c494b63ef50b91
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2024-08-02 15:18:09 -0700

    Updated template

--- packages/core/src/blocks/template.fragment.glsl
+++ packages/core/src/blocks/template.fragment.glsl
@@ -1,19 +1,18 @@
 // Rules for Smart Filter compatible shaders:
-// 1. It must contain a // [Smart Filter Shader Version] = 1 comment (the version may be different)
-// 2. There must be a sampler2D uniform designed as the main input texture (the one to be passed along)
-//    if this block is disabled.  It must be preceeced by a line starting with // main
-// 3. There must be a single main function which takes in a vec2 named vUV and returns a vec4, and 
-//    it must be proceeded by a line starting with // main
-// 4. Any uniforms which should have the same value across all instances of the same block should be 
-//    proceeded by a line starting with // single
+// 1. There must be a sampler2D uniform designed as the main input texture (the one to be passed along)
+//    if this block is disabled.  It must have a comment on its line like this // main
+// 2. There must be a single main function which takes in a vec2 named vUV and returns a vec4, and 
+//    it must have a comment on its line like this // main
+// 3. Any uniforms which should have the same value across all instances of the same block should 
+//    have a comment on its line like this // single
+// 4. Functions must be declared with the open { on the same line as the function name
 
-// main
-uniform sampler2D input;
-// single
-uniform float intensity;
+uniform sampler2D input; // main
+
+uniform float intensity; // single
+
 const float alpha = 0.9;
 
-// main
-vec4 makeRed(vec2 vUV) {
+vec4 makeRed(vec2 vUV) { // main
     return vec4(texture(input * intensity, vUV).r, 0., 0., alpha);
 }
