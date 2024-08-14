c34ac556c16016f6bbb42d4c9972676b4520fc07
Alex C. Huber <91097647+alexchuber@users.noreply.github.com>
2024-08-14 12:31:59 -0400

    Use interfaces & default values in custom desers

--- packages/demo/src/configuration/blocks/effects/blurBlock.deserializer.ts
+++ packages/demo/src/configuration/blocks/effects/blurBlock.deserializer.ts
@@ -1,15 +1,24 @@
 import type { ISerializedBlockV1, SmartFilter } from "@babylonjs/smart-filters";
 import { BlurBlock } from "./blurBlock";
 
+export interface ISerializedBlurBlockV1 extends ISerializedBlockV1 {
+    data: {
+        blurTextureRatioPerPass: number;
+        blurSize: number;
+    };
+}
+
 /**
  * V1 Blur Deserializer.
  * @param smartFilter - The SmartFilter to deserialize the block into
  * @param serializedBlock - The serialized block data
  * @returns A deserialized BlurBlock
  */
-export function blurBlockDeserializer(smartFilter: SmartFilter, serializedBlock: ISerializedBlockV1) {
+export function blurBlockDeserializer(smartFilter: SmartFilter, serializedBlock: ISerializedBlurBlockV1) {
     const block = new BlurBlock(smartFilter, serializedBlock.name);
-    block.blurTextureRatioPerPass = serializedBlock.data.blurTextureRatioPerPass;
-    block.blurSize = serializedBlock.data.blurSize;
+
+    // If data is missing, use the default value set by the block
+    block.blurTextureRatioPerPass = serializedBlock.data.blurTextureRatioPerPass ?? block.blurSize;
+    block.blurSize = serializedBlock.data.blurSize ?? block.blurSize;
     return block;
 }
--- packages/demo/src/configuration/blocks/transitions/wipeBlock.deserializer.ts
+++ packages/demo/src/configuration/blocks/transitions/wipeBlock.deserializer.ts
@@ -1,6 +1,13 @@
 import type { ISerializedBlockV1, SmartFilter } from "@babylonjs/smart-filters";
 import { WipeBlock } from "./wipeBlock";
 
+export interface ISerializedWipeBlockV1 extends ISerializedBlockV1 {
+    data: {
+        angle: number;
+        size: number;
+    };
+}
+
 /**
  * V1 Wipe Deserializer.
  * @param smartFilter - The SmartFilter to deserialize the block into
@@ -9,7 +16,9 @@ import { WipeBlock } from "./wipeBlock";
  */
 export function wipeDeserializer(smartFilter: SmartFilter, serializedBlock: ISerializedBlockV1) {
     const block = new WipeBlock(smartFilter, serializedBlock.name);
-    block.angle = serializedBlock.data.angle;
-    block.size = serializedBlock.data.size;
+
+    // If data is missing, use the default value set by the block
+    block.angle = serializedBlock.data.angle ?? block.angle;
+    block.size = serializedBlock.data.size ?? block.size;
     return block;
 }
