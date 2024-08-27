edac3448e202f8d7c20787d2b314aaa67db633e7
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2024-08-27 16:47:05 -0700

    Added MaskBlock and masked person

--- packages/demo/src/configuration/smartFilters/hardCoded/videoReaction.ts
+++ packages/demo/src/configuration/smartFilters/hardCoded/videoReaction.ts
@@ -8,10 +8,12 @@ import {
 } from "@babylonjs/smart-filters";
 import { HardCodedSmartFilterNames } from "./hardCodedSmartFilterNames";
 import { CompositionBlock } from "../../blocks/effects/compositionBlock";
+import { MaskBlock } from "../../blocks/effects/maskBlock";
 
 export function createVideoReactionSmartFilter(engine: ThinEngine): SmartFilter {
     const smartFilter = new SmartFilter(HardCodedSmartFilterNames.videoReaction);
 
+    // Inputs
     const backgroundTexture = createImageTexture(engine, "/assets/teamsBackground1.png");
     const backgroundInput = new InputBlock(
         smartFilter,
@@ -28,20 +30,26 @@ export function createVideoReactionSmartFilter(engine: ThinEngine): SmartFilter 
         createStrongRef(personTexture)
     );
 
+    const maskTexture = createImageTexture(engine, "/assets/personMask.png");
+    const maskInput = new InputBlock(smartFilter, "mask", ConnectionPointType.Texture, createStrongRef(maskTexture));
+
+    // MaskBlock
+    const maskBlock = new MaskBlock(smartFilter, "maskBlock");
+    personInput.output.connectTo(maskBlock.input);
+    maskInput.output.connectTo(maskBlock.mask);
+
+    // CompositionBlock
+    const compositionBlock = new CompositionBlock(smartFilter, "personOnBackground");
     const personTopInput = new InputBlock(smartFilter, "personTop", ConnectionPointType.Float, 0.0);
     const personLeftInput = new InputBlock(smartFilter, "personLeft", ConnectionPointType.Float, 0.0);
     const personWidthInput = new InputBlock(smartFilter, "personWidth", ConnectionPointType.Float, 1.0);
     const personHeightInput = new InputBlock(smartFilter, "personHeight", ConnectionPointType.Float, 1.0);
-
-    const compositionBlock = new CompositionBlock(smartFilter, "personOnBackground");
     personTopInput.output.connectTo(compositionBlock.foregroundTop);
     personLeftInput.output.connectTo(compositionBlock.foregroundLeft);
     personWidthInput.output.connectTo(compositionBlock.foregroundWidth);
     personHeightInput.output.connectTo(compositionBlock.foregroundHeight);
-
     backgroundInput.output.connectTo(compositionBlock.background);
-    personInput.output.connectTo(compositionBlock.foreground);
-
+    maskBlock.output.connectTo(compositionBlock.foreground);
     compositionBlock.output.connectTo(smartFilter.output);
 
     return smartFilter;
