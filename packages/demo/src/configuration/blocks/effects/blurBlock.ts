d2c20208de3f452a84ada78cb3b0b0bc438d55d5
Alex C. Huber <91097647+alexchuber@users.noreply.github.com>
2024-09-11 10:52:18 -0400

    Add getters/setters for BlurBlock static properties

--- packages/demo/src/configuration/blocks/effects/blurBlock.ts
+++ packages/demo/src/configuration/blocks/effects/blurBlock.ts
@@ -28,20 +28,49 @@ export class BlurBlock extends AggregateBlock {
      */
     public readonly output: ConnectionPoint<ConnectionPointType.Texture>;
 
-    /**
-     * Defines how smaller we should make the texture between the 2 consecutive bi lateral passes.
-     */
-    public blurTextureRatioPerPass = defaultBlurTextureRatioPerPass;
-
-    /**
-     * Defines how far the kernel might fetch the data from.
-     */
-    public blurSize = defaultBlurSize;
+    private _blurTextureRatioPerPass = defaultBlurTextureRatioPerPass;
+    private _blurSize = defaultBlurSize;
 
     private readonly _intermediateBlurV: DirectionalBlurBlock;
     private readonly _intermediateBlurH: DirectionalBlurBlock;
     private readonly _finalBlurV: DirectionalBlurBlock;
     private readonly _finalBlurH: DirectionalBlurBlock;
+
+    /**
+     * Gets how smaller we should make the texture between the 2 consecutive bi lateral passes.
+     */
+    public get blurTextureRatioPerPass(): number {
+        return this._blurTextureRatioPerPass;
+    }
+
+    /**
+     * Sets how smaller we should make the texture between the 2 consecutive bi lateral passes.
+     */
+    public set blurTextureRatioPerPass(value: number) {
+        this._blurTextureRatioPerPass = value;
+        this._intermediateBlurV.blurTextureRatio = value;
+        this._intermediateBlurH.blurTextureRatio = value;
+        this._finalBlurV.blurTextureRatio = value * value;
+        this._finalBlurH.blurTextureRatio = value * value;
+    }
+
+    /**
+     * Gets how far the kernel might fetch the data from.
+     */
+    public get blurSize(): number {
+        return this._blurSize;
+    }
+
+    /**
+     * Sets how far the kernel might fetch the data from.
+     */
+    public set blurSize(value: number) {
+        this._blurSize = value;
+        this._intermediateBlurV.blurHorizontalWidth = value;
+        this._intermediateBlurH.blurVerticalWidth = value;
+        this._finalBlurV.blurHorizontalWidth = value;
+        this._finalBlurH.blurVerticalWidth = value;
+    }
 
     /**
      * Instantiates a new Block.
