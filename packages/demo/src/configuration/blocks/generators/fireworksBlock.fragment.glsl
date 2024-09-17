4c2573a0c4621d59366d478710dcf61de4aaedd3
Alex C. Huber <91097647+alexchuber@users.noreply.github.com>
2024-09-17 17:11:53 -0400

    Replace FireworksBlock shader

--- packages/demo/src/configuration/blocks/generators/fireworksBlock.fragment.glsl
+++ packages/demo/src/configuration/blocks/generators/fireworksBlock.fragment.glsl
@@ -1,113 +1,74 @@
-// Source: https://www.shadertoy.com/view/4dBGRw
-// Author: Qqwy - 2013
+// Source: https://www.shadertoy.com/view/Ws3SRS
+// Author: piyushslayer - 2019
 // License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 // Modified from original
 // For demo and non-commerical use only
 
 uniform sampler2D input; // main
 uniform float time;
-uniform vec2 resolution;
+uniform float aspectRatio;
 
-const float eps = 0.1;
-const mat2 rr = mat2(cos(1.0), -sin(1.0), sin(1.0), cos(1.0));
+const float PI = 3.141592653589793;
+const float EXPLOSION_COUNT = 8.;
+const float SPARKS_PER_EXPLOSION = 128.;
+const float EXPLOSION_DURATION = 20.;
+const float EXPLOSION_SPEED = 5.;
+const float EXPLOSION_RADIUS_THESHOLD = .06;
 
-//Creates a diagonal red-and-white striped pattern.
-vec3 barberpole(vec2 pos, vec2 rocketpos) {
-	float d = (pos.x-rocketpos.x)+(pos.y-rocketpos.y);
-	vec3 col=vec3(1.0);	
-
-	d = mod(d*20.,2.0);
-	if(d>1.0){
-		col=vec3(1.0,0.0,0.0);
-	}
-
-	return col;	
+// Hash function by Dave_Hoskins.
+const vec3 MOD3 = vec3(.1031,.11369,.13787);
+vec3 hash31(float p) {
+   vec3 p3 = fract(vec3(p) * MOD3);
+   p3 += dot(p3, p3.yzx + 19.19);
+   return fract(vec3((p3.x + p3.y) * p3.z, (p3.x + p3.z) * p3.y, (p3.y + p3.z) * p3.x));
 }
 
-
-vec3 rocket(vec2 pos, vec2 rocketpos){
-	vec3 col = vec3(0.0);
-	float f = 0.;
-	float absx= abs(rocketpos.x - pos.x);
-	float absy = abs(rocketpos.y-pos.y);
-	//wooden stick
-	if(absx<0.01&&absy<0.22){
-		col=vec3(1.0,0.5,0.5);	
-	}
-	
-	//Barberpole
-	
-	if(absx<0.05&&absy<0.15){
-		col=barberpole(pos, rocketpos);	
-	}
-	//Rocket Point
-	float pointw=(rocketpos.y-pos.y-0.25)*-0.7;
-	if((rocketpos.y-pos.y)>0.1){
-		f=smoothstep(pointw-0.001,pointw+0.001,absx);
-		
-		col=mix(vec3(1.0,0.0,0.0),col, f);	
-	}
-	//Shadow
-	
-	f =-.5 + smoothstep(-0.05, 0.05, (rocketpos.x-pos.x));
-	col*= 0.7+f;
-	
-	return col;
-}
-
-
-
-float rand(float val, float seed){
-	return cos(val*sin(val*seed)*seed);	
-}
-
-float distance2( in vec2 a, in vec2 b ) { return dot(a-b,a-b); }
-
-vec3 drawParticles(vec2 pos, vec3 particolor, float time, vec2 cpos, float gravity, float seed, float timelength){
-    vec3 col= vec3(0.0);
-    vec2 pp = vec2(1.0,0.0);
-    for(float i=1.0;i<=128.0;i++){
-        float d=rand(i, seed);
-        float fade=(i/128.0)*time;
-        vec2 particpos = cpos + time*pp*d;
-        pp = rr*pp;
-        col = mix(particolor/fade, col, smoothstep(0.0, 0.0001, distance2(particpos, pos)));
+vec4 mainImage(vec2 vUV) { // main
+    float t = mod(time + 10., 7200.);
+	vec4 col = texture2D(input, vUV); 
+    vec2 origin = vec2(0.);
+	vUV.x *= aspectRatio;
+    
+    for (float j = 0.; j < EXPLOSION_COUNT; ++j)
+    {
+        vec3 oh = hash31((j + 1234.1939) * 641.6974);
+        origin = vec2(oh.x, oh.y) * .6 + .2; // .2 - .8 to avoid boundaries
+        origin.x *= aspectRatio;
+        // Change t value to randomize the spawning of explosions
+        t += (j + 1.) * 9.6491 * oh.z;
+        for (float i = 0.; i < SPARKS_PER_EXPLOSION; ++i)
+    	{
+            vec3 h = hash31(j * 963.31 + i + 497.8943);
+            // random angle (0 - 2*PI)
+            float a = h.x * PI * 2.;
+            // random radius scale for spawning points anywhere in a circle
+            float rScale = h.y * EXPLOSION_RADIUS_THESHOLD;
+            // explosion loop based on time
+            if (mod(t * EXPLOSION_SPEED, EXPLOSION_DURATION) > 2.)
+            {
+                // random radius 
+                float r = mod(t * EXPLOSION_SPEED, EXPLOSION_DURATION) * rScale;
+                // explosion spark polar coords 
+                vec2 sparkPos = vec2(r * cos(a), r * sin(a));
+                // fake-ish gravity
+                float foo = 0.04;
+                float bar = (length(sparkPos) - (rScale - foo)) / foo;
+                sparkPos.y -= pow(bar, 3.0) * 6e-5;
+                // shiny spark particles
+                float dist = length(vUV - sparkPos - origin);
+                float spark = .00015 / pow(dist, 1.65);
+                // Make the explosion spark shimmer/sparkle
+                float sd = 2. * length(origin-sparkPos);
+                float shimmer = max(0., sqrt(sd) * (sin((t + h.y * 2. * PI) * 20.)));
+                float shimmerThreshold = EXPLOSION_DURATION * .32;
+                // fade the particles towards the end of explosion
+                float fade = max(0., (EXPLOSION_DURATION - 5.) * rScale - r);
+                // mix it all together
+                vec3 sparkColor = spark * mix(1., shimmer, smoothstep(shimmerThreshold * rScale,
+					(shimmerThreshold + 1.) * rScale , r)) * fade * oh;
+				col.rgb = 1.0 - (1.0 - col.rgb) * (1.0 - sparkColor); // Screen blending for better result
+            }
+    	}
     }
-    col*=smoothstep(0.0,1.0,(timelength-time)/timelength);
-	
     return col;
-}
-
-vec4 drawFireworks(float time, vec2 uv, vec3 particolor, float seed){
-	
-	float timeoffset = 2.0;
-	vec3 col=vec3(0.0);
-	if(time<=0.){
-		return vec4(col, 0.0);	
-	}
-	if(mod(time, 6.0)>timeoffset){
-	    col= drawParticles(uv, particolor, mod(time, 6.0)-timeoffset, vec2(rand(ceil(time/6.0),seed),-0.5), 0.5, ceil(time/6.0), seed);
-	}else{
-		
-		col= rocket(uv*3., vec2(3.*rand(ceil(time/6.0),seed),3.*(-0.5+(timeoffset-mod(time, 6.0)))));	
-	}
-
-    if (col.x <= eps && col.y<= eps && col.z <= eps) {
-        return vec4(0.0, 0.0, 0.0, 0.0);
-    }
-
-	return vec4(col, 1.0);	
-}
-
-vec4 mainImage (vec2 vUV) { // main
-	vec4 col = texture(input, vUV);
-	
-	vUV = 1.0 -  2.0 * vUV;
-	vUV.x *= resolution.x/resolution.y;
-	
-	col += drawFireworks(time    , vUV,vec3(1.0,0.1,0.1), 1.);
-	col += drawFireworks(time-2.0, vUV,vec3(0.0,1.0,0.5), 2.);
-	col += drawFireworks(time-4.0, vUV,vec3(1.0,1.0,0.1), 3.);
-	
-    return col;
-}
+} 
