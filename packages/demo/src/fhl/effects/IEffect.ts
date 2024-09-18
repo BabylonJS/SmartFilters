026b8d44ff29a0a634724c3a863a9d1e5379b072
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2024-09-18 10:52:53 -0700

    ESLint fix

--- packages/demo/src/fhl/effects/IEffect.ts
+++ packages/demo/src/fhl/effects/IEffect.ts
@@ -1,9 +1,9 @@
-import { Observable } from "@babylonjs/core/Misc/observable";
+import type { Observable } from "@babylonjs/core/Misc/observable";
 
 export interface IEffect {
-  readonly isStarted: boolean;
-  start(): void;
-  stop(notifyEffectCompleted: boolean): void;
+    readonly isStarted: boolean;
+    start(): void;
+    stop(notifyEffectCompleted: boolean): void;
 
-  readonly onEffectCompleted: Observable<void>;
+    readonly onEffectCompleted: Observable<void>;
 }
