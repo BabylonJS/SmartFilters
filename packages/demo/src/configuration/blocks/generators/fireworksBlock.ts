fe0e78af1aa8f5f355fe623a9d8465ec3a0254c5
Alex C. Huber <91097647+alexchuber@users.noreply.github.com>
2024-09-18 15:57:36 -0400

    Expose firework + spark count in FireworksBlock; update seed

--- packages/demo/src/configuration/blocks/generators/fireworksBlock.ts
+++ packages/demo/src/configuration/blocks/generators/fireworksBlock.ts
@@ -11,21 +11,29 @@ import { shaderProgram, uniforms } from "../generators/fireworksBlock.shader";
 export class FireworksShaderBinding extends ShaderBinding {
     private readonly _inputTexture: RuntimeData<ConnectionPointType.Texture>;
     private readonly _time: RuntimeData<ConnectionPointType.Float>;
+    private readonly _fireworks: RuntimeData<ConnectionPointType.Float>;
+    private readonly _fireworkSparks: RuntimeData<ConnectionPointType.Float>;
 
     /**
      * Creates a new shader binding instance for the Fireworks block.
      * @param parentBlock - The parent block
      * @param inputTexture - The input texture
      * @param time - The time passed since the start of the effect
+     * @param fireworks - The number of fireworks to display
+     * @param fireworkSparks - The number of sparks per firework
      */
     constructor(
         parentBlock: IDisableableBlock,
         inputTexture: RuntimeData<ConnectionPointType.Texture>,
-        time: RuntimeData<ConnectionPointType.Float>
+        time: RuntimeData<ConnectionPointType.Float>,
+        fireworks: RuntimeData<ConnectionPointType.Float>,
+        fireworkSparks: RuntimeData<ConnectionPointType.Float>
     ) {
         super(parentBlock);
         this._inputTexture = inputTexture;
         this._time = time;
+        this._fireworks = fireworks;
+        this._fireworkSparks = fireworkSparks;
     }
 
     /**
@@ -39,6 +47,8 @@ export class FireworksShaderBinding extends ShaderBinding {
         effect.setTexture(this.getRemappedName(uniforms.input), this._inputTexture.value);
         effect.setFloat(this.getRemappedName(uniforms.aspectRatio), _width / _height);
         effect.setFloat(this.getRemappedName(uniforms.time), this._time.value);
+        effect.setFloat(this.getRemappedName(uniforms.fireworks), this._fireworks.value);
+        effect.setFloat(this.getRemappedName(uniforms.fireworkSparks), this._fireworkSparks.value);
     }
 }
 
@@ -62,6 +72,24 @@ export class FireworksBlock extends ShaderBlock {
     public readonly time = this._registerOptionalInput("time", ConnectionPointType.Float, createStrongRef(0.3));
 
     /**
+     * The number of fireworks at a time, as a connection point.
+     */
+    public readonly fireworks = this._registerOptionalInput(
+        "fireworks",
+        ConnectionPointType.Float,
+        createStrongRef(8.0)
+    );
+
+    /**
+     * The number of sparks per firework, as a connection point.
+     */
+    public readonly fireworkSparks = this._registerOptionalInput(
+        "fireworkSparks",
+        ConnectionPointType.Float,
+        createStrongRef(128.0)
+    );
+
+    /**
      * The shader program (vertex and fragment code) to use to render the block
      */
     public static override ShaderCode = shaderProgram;
@@ -82,7 +110,9 @@ export class FireworksBlock extends ShaderBlock {
     public getShaderBinding(): ShaderBinding {
         const input = this._confirmRuntimeDataSupplied(this.input);
         const time = this.time.runtimeData;
+        const fireworks = this.fireworks.runtimeData;
+        const fireworkSparks = this.fireworkSparks.runtimeData;
 
-        return new FireworksShaderBinding(this, input, time);
+        return new FireworksShaderBinding(this, input, time, fireworks, fireworkSparks);
     }
 }
