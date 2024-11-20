eb4f129be813811d823c7cf49b2a015590a8408f
Deltakosh <david.catuhe@live.fr>
2024-11-19 17:04:24 -0800

    Enabled CTRL+SPACE

--- packages/editor/src/components/nodeList/nodeListComponent.tsx
+++ packages/editor/src/components/nodeList/nodeListComponent.tsx
@@ -4,6 +4,8 @@ import type { GlobalState } from "../../globalState";
 
 import { LineContainerComponent } from "../../sharedComponents/lineContainerComponent.js";
 import { DraggableLineComponent } from "../../sharedComponents/draggableLineComponent.js";
+
+import { NodeLedger } from "@babylonjs/shared-ui-components/nodeGraphSystem/nodeLedger.js";
 // import { DraggableLineWithButtonComponent } from "../../sharedComponents/draggableLineWithButtonComponent";
 // import { LineWithFileButtonComponent } from "../../sharedComponents/lineWithFileButtonComponent";
 
@@ -23,23 +25,10 @@ interface INodeListComponentProps {
 export class NodeListComponent extends react.Component<INodeListComponentProps, { filter: string }> {
     private _onResetRequiredObserver: Nullable<Observer<boolean>>;
 
-    // private _customFrameList: { [key: string]: string };
-    // private _customBlockList: { [key: string]: string };
-
     constructor(props: INodeListComponentProps) {
         super(props);
 
         this.state = { filter: "" };
-
-        // const frameJson = localStorage.getItem("Custom-Frame-List");
-        // if (frameJson) {
-        //     this._customFrameList = JSON.parse(frameJson);
-        // }
-
-        // const blockJson = localStorage.getItem("Custom-Block-List");
-        // if (blockJson) {
-        //     this._customBlockList = JSON.parse(blockJson);
-        // }
 
         this._onResetRequiredObserver = this.props.globalState.onResetRequiredObservable.add(() => {
             this.forceUpdate();
@@ -54,138 +43,17 @@ export class NodeListComponent extends react.Component<INodeListComponentProps, 
         this.setState({ filter: filter });
     }
 
-    // loadCustomFrame(file: File) {
-    //     Tools.ReadFile(
-    //         file,
-    //         async (data) => {
-    //             // get Frame Data from file
-    //             const decoder = new TextDecoder("utf-8");
-    //             const frameData = JSON.parse(decoder.decode(data));
-    //             const frameName = frameData.editorData.frames[0].name + "Custom";
-    //             const frameToolTip = frameData.editorData.frames[0].comments || "";
-
-    //             try {
-    //                 localStorage.setItem(frameName, JSON.stringify(frameData));
-    //             } catch (error) {
-    //                 this.props.globalState.stateManager.onErrorMessageDialogRequiredObservable.notifyObservers("Error Saving Frame");
-    //                 return;
-    //             }
-
-    //             const frameJson = localStorage.getItem("Custom-Frame-List");
-    //             let frameList: { [key: string]: string } = {};
-    //             if (frameJson) {
-    //                 frameList = JSON.parse(frameJson);
-    //             }
-    //             frameList[frameName] = frameToolTip;
-    //             localStorage.setItem("Custom-Frame-List", JSON.stringify(frameList));
-    //             this._customFrameList = frameList;
-    //             this.forceUpdate();
-    //         },
-    //         undefined,
-    //         true
-    //     );
-    // }
-
-    // removeItem(value: string): void {
-    //     const frameJson = localStorage.getItem("Custom-Frame-List");
-    //     if (frameJson) {
-    //         const frameList = JSON.parse(frameJson);
-    //         delete frameList[value];
-    //         localStorage.removeItem(value);
-    //         localStorage.setItem("Custom-Frame-List", JSON.stringify(frameList));
-    //         this._customFrameList = frameList;
-    //         this.forceUpdate();
-    //     }
-    // }
-
-    // loadCustomBlock(file: File) {
-    //     Tools.ReadFile(
-    //         file,
-    //         async (data) => {
-    //             // get Block Data from file
-    //             const decoder = new TextDecoder("utf-8");
-    //             const blockData = JSON.parse(decoder.decode(data));
-    //             const blockName = (blockData.name || "") + "CustomBlock";
-    //             const blockToolTip = blockData.comments || "";
-
-    //             try {
-    //                 localStorage.setItem(blockName, JSON.stringify(blockData));
-    //             } catch (error) {
-    //                 this.props.globalState.stateManager.onErrorMessageDialogRequiredObservable.notifyObservers("Error Saving Block");
-    //                 return;
-    //             }
-
-    //             const blockJson = localStorage.getItem("Custom-Block-List");
-    //             let blockList: { [key: string]: string } = {};
-    //             if (blockJson) {
-    //                 blockList = JSON.parse(blockJson);
-    //             }
-    //             blockList[blockName] = blockToolTip;
-    //             localStorage.setItem("Custom-Block-List", JSON.stringify(blockList));
-    //             this._customBlockList = blockList;
-    //             this.forceUpdate();
-    //         },
-    //         undefined,
-    //         true
-    //     );
-    // }
-
-    // removeCustomBlock(value: string): void {
-    //     const blockJson = localStorage.getItem("Custom-Block-List");
-    //     if (blockJson) {
-    //         const blockList = JSON.parse(blockJson);
-    //         delete blockList[value];
-    //         localStorage.removeItem(value);
-    //         localStorage.setItem("Custom-Block-List", JSON.stringify(blockList));
-    //         this._customBlockList = blockList;
-    //         this.forceUpdate();
-    //     }
-    // }
-
     override render() {
-        // const customFrameNames: string[] = [];
-        // for (const frame in this._customFrameList) {
-        //     customFrameNames.push(frame);
-        // }
-
-        // const customBlockNames: string[] = [];
-        // for (const block in this._customBlockList) {
-        //     customBlockNames.push(block);
-        // }
-
         // Create node menu
         const blockMenu = [];
-        for (const key in this.props.globalState.blockRegistration.allBlockNames) {
+        const allBlocks = this.props.globalState.blockRegistration.allBlockNames;
+        for (const key in allBlocks) {
             const blockList = (this.props.globalState.blockRegistration.allBlockNames as any)[key]
                 .filter(
                     (b: string) => !this.state.filter || b.toLowerCase().indexOf(this.state.filter.toLowerCase()) !== -1
                 )
                 .sort((a: string, b: string) => a.localeCompare(b))
                 .map((block: string) => {
-                    // if (key === "Custom_Frames") {
-                    //     return (
-                    //         <DraggableLineWithButtonComponent
-                    //             key={block}
-                    //             data={block}
-                    //             tooltip={this._customFrameList[block] || ""}
-                    //             iconImage={deleteButton}
-                    //             iconTitle="Delete"
-                    //             onIconClick={(value) => this.removeItem(value)}
-                    //         />
-                    //     );
-                    // } else if (key === "Custom_Blocks") {
-                    //     return (
-                    //         <DraggableLineWithButtonComponent
-                    //             key={block}
-                    //             data={block}
-                    //             tooltip={this._customBlockList[block] || ""}
-                    //             iconImage={deleteButton}
-                    //             iconTitle="Delete"
-                    //             onIconClick={(value) => this.removeCustomBlock(value)}
-                    //             lenSuffixToRemove={11}
-                    //         />
-                    //     );
-                    // }
                     return (
                         <DraggableLineComponent
                             key={block}
@@ -195,39 +63,6 @@ export class NodeListComponent extends react.Component<INodeListComponentProps, 
                     );
                 });
 
-            // if (key === "Custom_Frames") {
-            //     const line = (
-            //         <LineWithFileButtonComponent
-            //             key="add..."
-            //             title={"Add Custom Frame"}
-            //             closed={false}
-            //             label="Add..."
-            //             uploadName={"custom-frame-upload"}
-            //             iconImage={addButton}
-            //             accept=".json"
-            //             onIconClick={(file) => {
-            //                 this.loadCustomFrame(file);
-            //             }}
-            //         />
-            //     );
-            //     blockList.push(line);
-            // } else if (key === "Custom_Blocks") {
-            //     const line = (
-            //         <LineWithFileButtonComponent
-            //             key="add..."
-            //             title={"Add Custom Block"}
-            //             closed={false}
-            //             label="Add..."
-            //             uploadName={"custom-block-upload"}
-            //             iconImage={addButton}
-            //             accept=".json"
-            //             onIconClick={(file) => {
-            //                 this.loadCustomBlock(file);
-            //             }}
-            //         />
-            //     );
-            //     blockList.push(line);
-            // }
             if (blockList.length) {
                 blockMenu.push(
                     <LineContainerComponent
@@ -240,6 +75,31 @@ export class NodeListComponent extends react.Component<INodeListComponentProps, 
                 );
             }
         }
+
+        // Register blocks
+        const ledger = NodeLedger.RegisteredNodeNames;
+        for (const key in allBlocks) {
+            const blocks = allBlocks[key] as string[];
+            if (blocks.length) {
+                for (const block of blocks) {
+                    if (!ledger.includes(block)) {
+                        ledger.push(block);
+                    }
+                }
+            }
+        }
+        NodeLedger.NameFormatter = (name) => {
+            let finalName = name;
+            // custom frame
+            if (name.endsWith("Custom")) {
+                const nameIndex = name.lastIndexOf("Custom");
+                finalName = name.substring(0, nameIndex);
+                finalName += " [custom]";
+            } else {
+                finalName = name.replace("Block", "");
+            }
+            return finalName;
+        };
 
         return (
             <div id="nodeList">
