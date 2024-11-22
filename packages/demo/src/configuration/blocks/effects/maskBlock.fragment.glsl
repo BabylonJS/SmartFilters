11c916f773efafdb8680c7691fd26f0e4abcf511
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2024-11-22 13:28:26 -0800

    Mask demo block: respect alpha of input texture (#68)

--- packages/demo/src/configuration/blocks/effects/maskBlock.fragment.glsl
+++ packages/demo/src/configuration/blocks/effects/maskBlock.fragment.glsl
@@ -2,9 +2,9 @@ uniform sampler2D input; // main
 uniform sampler2D mask;
 
 vec4 maskBlock(vec2 vUV) { // main
-    vec3 color = texture2D(input, vUV).rgb;
+    vec4 color = texture2D(input, vUV);
     vec3 maskColor = texture2D(mask, vUV).rgb;
     float luminance = dot(maskColor, vec3(0.3, 0.59, 0.11));
 
-    return vec4(color * luminance, luminance);
+    return vec4(color.rgb * luminance, luminance * color.a);
 }
