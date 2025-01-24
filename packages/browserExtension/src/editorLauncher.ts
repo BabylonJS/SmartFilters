82db6975c4e457b0a271024e2f5149e6e2d8adcc
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-01-23 16:17:33 -0800

    Console log when launching

--- packages/browserExtension/src/editorLauncher.ts
+++ packages/browserExtension/src/editorLauncher.ts
@@ -4,9 +4,12 @@ const smartFilter = (window as any).currentSmartFilter;
 const thinEngine = (window as any).thinEngine;
 
 if (smartFilter) {
+    console.log("A SmartFilter was found in this document, launching the editor");
     // Display the editor
     SmartFilterEditor.Show({
         engine: thinEngine,
         filter: smartFilter,
     });
+} else {
+    console.log("No SmartFilter was found in this document");
 }
