49158a221445b2b0644f008e6fe0cf81c8992846
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-01-28 11:12:22 -0800

    Now doing bindings

--- packages/core/src/blocks/customShaderBlock.ts
+++ packages/core/src/blocks/customShaderBlock.ts
@@ -1,5 +1,6 @@
-import type { ConnectionPointType } from "../connection/connectionPointType";
-import type { ShaderBinding } from "../runtime/shaderRuntime";
+import type { Effect } from "@babylonjs/core/Materials/effect";
+import { ConnectionPointType } from "../connection/connectionPointType.js";
+import { ShaderBinding } from "../runtime/shaderRuntime.js";
 import { createStrongRef } from "../runtime/strongRef.js";
 import type { SerializedBlockDefinition } from "../serialization/serializedBlockDefinition";
 import type {
@@ -9,6 +10,54 @@ import type {
 import type { SmartFilter } from "../smartFilter";
 // import type { ShaderProgram } from "../utils/shaderCodeUtils";
 import { ShaderBlock } from "./shaderBlock.js";
+import type { RuntimeData } from "../connection/connectionPoint";
+
+/**
+ * The binding for a CustomShaderBlock
+ */
+class CustomShaderBlockBinding extends ShaderBinding {
+    private readonly _inputsWithRuntimeData: AnyInputWithRuntimeData[];
+
+    /**
+     * Creates a new shader binding instance for the CustomShaderBlock block.
+     * @param inputsWithRuntimeData - The input connection points of the block
+     */
+    public constructor(inputsWithRuntimeData: AnyInputWithRuntimeData[]) {
+        super();
+        this._inputsWithRuntimeData = inputsWithRuntimeData;
+    }
+
+    /**
+     * Binds all the required data to the shader when rendering.
+     * @param effect - defines the effect to bind the data to
+     */
+    public override bind(effect: Effect): void {
+        let remappedName: string;
+        for (const input of this._inputsWithRuntimeData) {
+            remappedName = this.getRemappedName(input.name);
+            switch (input.type) {
+                case ConnectionPointType.Float:
+                    effect.setFloat(remappedName, input.runtimeData.value);
+                    break;
+                case ConnectionPointType.Texture:
+                    effect.setTexture(remappedName, input.runtimeData.value);
+                    break;
+                case ConnectionPointType.Color3:
+                    effect.setColor3(remappedName, input.runtimeData.value);
+                    break;
+                case ConnectionPointType.Color4:
+                    effect.setDirectColor4(remappedName, input.runtimeData.value);
+                    break;
+                case ConnectionPointType.Boolean:
+                    effect.setBool(remappedName, input.runtimeData.value);
+                    break;
+                case ConnectionPointType.Vector2:
+                    effect.setVector2(remappedName, input.runtimeData.value);
+                    break;
+            }
+        }
+    }
+}
 
 /**
  * A block which loads a SerializedBlockDefinition for use in a SmartFilter.
@@ -90,10 +139,51 @@ export class CustomShaderBlock extends ShaderBlock {
     }
 
     /**
-     * TODO: do we need to keep this?
+     * Gets the shader binding for the custom shader block.
+     * @returns The shader binding for the custom shader block
      */
     public override getShaderBinding(): ShaderBinding {
-        throw new Error("Not implemented");
-        // return new SerializedShaderBlockShaderBinding(this, this._shaderProgram);
+        const inputs = this.inputs;
+
+        const inputsWithRuntimeData = inputs.map((input) => {
+            return {
+                name: input.name,
+                type: input.type,
+                runtimeData: this._confirmRuntimeDataSupplied(input),
+            };
+        });
+
+        return new CustomShaderBlockBinding(inputsWithRuntimeData);
     }
 }
+
+/**
+ * Represents an input with its runtime data, enforcing type safety.
+ */
+type InputWithRuntimeData<U extends ConnectionPointType> = {
+    /**
+     * asdf
+     */
+    name: string;
+
+    /**
+     * asdf
+     */
+    type: U;
+
+    /**
+     * asdf
+     */
+    runtimeData: RuntimeData<U>;
+};
+
+/**
+ * All possible input types with runtime data.
+ */
+type AnyInputWithRuntimeData =
+    | InputWithRuntimeData<ConnectionPointType.Boolean>
+    | InputWithRuntimeData<ConnectionPointType.Color3>
+    | InputWithRuntimeData<ConnectionPointType.Color4>
+    | InputWithRuntimeData<ConnectionPointType.Float>
+    | InputWithRuntimeData<ConnectionPointType.Texture>
+    | InputWithRuntimeData<ConnectionPointType.Vector2>;
