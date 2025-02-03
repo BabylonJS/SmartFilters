878ff8df94fe828c9c35d4c996f7e60be52db25f
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-02-03 08:20:29 -0800

    build working

---
+++ packages/sfe/build/preparePublish.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+const corePackageText = fs.readFileSync("../core/package.json");
+const corePackageJSON = JSON.parse(corePackageText.toString());
+
+let versionFileContents = fs.readFileSync("./www/version.json").toString();
+console.log("Setting version to display to the core version:", corePackageJSON.version);
+
+versionFileContents = versionFileContents.replace("Locally Built", `Version: ${corePackageJSON.version}`);
+
+console.log("Saving changes to version.json");
+fs.writeFileSync("./www/version.json", versionFileContents);
---
+++ packages/sfe/src/app.ts
@@ -0,0 +1,2 @@
+export const foo = 42;
+console.log("Hello, world!");
