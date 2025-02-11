26a0adc0be303776c71a7d642b5f63d962a24271
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-02-11 15:48:10 -0800

    Need to figure out async work

--- packages/core/src/blocks/customAggregateBlock.ts
+++ packages/core/src/blocks/customAggregateBlock.ts
@@ -1,4 +1,10 @@
-import type { SerializedSmartFilterV1 } from "../serialization/index.js";
+import type { ThinEngine } from "@babylonjs/core/Engines/thinEngine.js";
+import {
+    SmartFilterDeserializer,
+    type BlockFactory,
+    type OptionalBlockDeserializerV1,
+    type SerializedSmartFilterV1,
+} from "../serialization/index.js";
 import type { SmartFilter } from "../smartFilter.js";
 import { AggregateBlock } from "./aggregateBlock.js";
 import type { BaseBlock } from "./baseBlock.js";
@@ -9,16 +15,29 @@ import type { BaseBlock } from "./baseBlock.js";
 export class CustomAggregateBlock extends AggregateBlock {
     /**
      * Creates a new CustomAggregateBlock
-     * @param _smartFilter - The SmartFilter to create the block for
-     * @param _name - The name of the block
-     * @param _blockDefinition - The serialized block definition
+     * @param smartFilter - The SmartFilter to create the block for
+     * @param engine - The ThinEngine to use
+     * @param name - The name of the block
+     * @param serializedSmartFilter - The serialized SmartFilter to load into the block
+     * @param blockFactory - A function that creates a block of the given class name, or returns null if it cannot
+     * @param customInputBlockDeserializer - An optional custom deserializer for InputBlocks - if supplied and it returns null, the default deserializer will be used
+     * @returns A promise that resolves to the new CustomAggregateBlock
      */
-    public static Create(
-        _smartFilter: SmartFilter,
-        _name: string,
-        _blockDefinition: SerializedSmartFilterV1
-    ): BaseBlock {
-        throw new Error("Method not implemented.");
+    public static async Create(
+        smartFilter: SmartFilter,
+        engine: ThinEngine,
+        name: string,
+        serializedSmartFilter: SerializedSmartFilterV1,
+        blockFactory: BlockFactory,
+        customInputBlockDeserializer?: OptionalBlockDeserializerV1
+    ): Promise<BaseBlock> {
+        const deserializer = new SmartFilterDeserializer(blockFactory, customInputBlockDeserializer);
+        const innerSmartFilter = await deserializer.deserialize(engine, serializedSmartFilter);
+        return new CustomAggregateBlock(smartFilter, engine, name, innerSmartFilter);
+    }
+
+    private constructor(smartFilter: SmartFilter, engine: ThinEngine, name: string, innerSmartFilter: SmartFilter) {
+        super(name);
     }
 
     /**
