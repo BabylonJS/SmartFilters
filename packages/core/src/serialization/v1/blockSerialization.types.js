277b11d0b7104430aa9d0d957eb5e03fc7c1689a
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-02-10 19:12:02 -0800

    Remove files that shouldn't be there

--- packages/core/src/serialization/v1/blockSerialization.types.js
+++
@@ -1,7 +0,0 @@
-/**
- * ----------------------------------------------------------------------------
- * Data Types Used For Block Serialization
- * ----------------------------------------------------------------------------
- */
-export {};
-//# sourceMappingURL=blockSerialization.types.js.map
