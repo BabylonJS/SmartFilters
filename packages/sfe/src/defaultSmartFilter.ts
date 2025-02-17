6a98ab80a850ed37c7d11de5291ee2cbea8fd8de
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-02-16 19:00:46 -0800

    Added texture input in default graph

--- packages/sfe/src/defaultSmartFilter.ts
+++ packages/sfe/src/defaultSmartFilter.ts
@@ -9,6 +9,16 @@ export function createDefaultSmartFilter(): SmartFilter {
     const smartFilter = new SmartFilter("Default");
 
     const pixelateBlock = new PixelateBlock(smartFilter, "Pixelate");
+
+    const textureInputBlock = new InputBlock(smartFilter, "Texture", ConnectionPointType.Texture, null);
+    textureInputBlock.editorData = {
+        url: "/assets/logo.png",
+        urlTypeHint: "image",
+        flipY: true,
+        anisotropicFilteringLevel: null,
+        forcedExtension: null,
+    };
+
     const intensityInputBlock = new InputBlock(smartFilter, "Intensity", ConnectionPointType.Float, 0.5);
     intensityInputBlock.editorData = {
         animationType: null,
@@ -17,6 +27,7 @@ export function createDefaultSmartFilter(): SmartFilter {
         max: 1.0,
     };
 
+    textureInputBlock.output.connectTo(pixelateBlock.input);
     intensityInputBlock.output.connectTo(pixelateBlock.intensity);
     pixelateBlock.output.connectTo(smartFilter.output);
 
