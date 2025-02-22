b551835027fa7682823b5d9e95103971ed05dcd2
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-02-22 11:42:01 -0800

    Core version export

--- packages/core/src/utils/buildTools/versionUp.ts
+++ packages/core/src/utils/buildTools/versionUp.ts
@@ -41,6 +41,7 @@ queryNpmFeed("preview", (npmPreviewVersion) => {
 
         console.log("Version to use:", versionToUse);
 
+        // Update package.json if needed
         if (packageJSON.version !== versionToUse) {
             packageJSON.version = versionToUse;
             fs.writeFileSync("package.json", JSON.stringify(packageJSON, null, 4));
@@ -48,5 +49,12 @@ queryNpmFeed("preview", (npmPreviewVersion) => {
         } else {
             console.log("No need to update package.json");
         }
+
+        // Write out to version.ts
+        const versionTsText = `/**
+ * The version of the SmartFilter core. During publish, this file is overwritten by versionUp.ts with the same version that is used for the NPM publish.
+ */
+export const SmartFilterCoreVersion = "${versionToUse}";\n`;
+        fs.writeFileSync("src/version.ts", versionTsText);
     });
 });
