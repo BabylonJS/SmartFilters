5ea0450e7a27fcaafc1c6bb45ed0c35955078c0e
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-02-27 15:04:43 -0800

    Simple wipe with no angle

--- packages/core/src/blocks/transitions/wipeBlock.ts
+++ packages/core/src/blocks/transitions/wipeBlock.ts
@@ -22,7 +22,6 @@ const shaderProgram: ShaderProgram = {
             uniform sampler2D _textureA_;
             uniform sampler2D _textureB_;
             uniform float _mix_;
-            uniform float _angle_;
             `,
 
         mainFunctionName: "_wipe_",
@@ -35,58 +34,9 @@ const shaderProgram: ShaderProgram = {
                 code: `
                 vec4 _wipe_(vec2 vUV)
                 {
-                    float radians = radians(_angle_);
                     vec4 colorA = texture2D(_textureA_, vUV);
                     vec4 colorB = texture2D(_textureB_, vUV);
-
-                    // float isAboveLine = step(1.0 - _mix_, vUV.y);// step(vUV.x * tan(radians), vUV.y);
-
-                    float tanAngle = tan(radians);
-                    float yAtMiddle = 0.5 * tanAngle;
-                    float yIntercept = 0.5 - yAtMiddle;
-                    float isAboveLine = step(vUV.x * tan(radians), vUV.y);
-                    float lineY = (vUV.x * tanAngle) + yIntercept;
-
-                    float blendDistance = 0.01;
-                    float distanceFromLine = clamp((vUV.y - lineY) * acos(radians), -1.0 * blendDistance, 1.0) +blendDistance;
-                   
-                    float mixA = clamp(blendDistance - distanceFromLine, 0.0, blendDistance) / blendDistance;
-                    return mix(colorA, colorB, mixA);
-                    //return vec4(abs(mix)); 
-
-                    // float blendAmount = 0.01;
-                    // float mixA = clamp(0.5 - (blendAmount - distanceFromLine), 0.0, 0.5) + 0.5; // clamp(distanceFromLine, 0.0, 1.0);
-                    // return vec4(abs(mixA));
-
-                    // return mix(colorA, colorB, mixA);
-
-                    //return mix(colorA, colorB, isAboveLine);
-                    //return vec4(vUV.x * tan(radians));
-                
-
-
-                    // float a = acos(cos(_angle_)); // Bring angle into range of 0-pi for ease
-                    // float isHalfPi = step(abs(a - HALF_PI), 0.0);
-                    // float isGreaterThanHalfPi = 1. - step(a, HALF_PI);
-                    // float isLessThanHalfPi = 1. - step(HALF_PI, a);
-
-                    // // Build a line equation, D = Ax + By + C
-                    // // There are 3 possible line equations based on the angle of the wipe
-                    // float m = tan(a); // Convert angle into line slope
-                    // float A = mix(-m, -LARGE, isHalfPi);
-                    // float C = isLessThanHalfPi * -(1. - _mix_ - (_mix_ * m));
-                    // C += isGreaterThanHalfPi * -(_mix_ - (_mix_ * m));
-                    // C += isHalfPi * (LARGE * _mix_);
-                    
-                    // // Solve for D using UV coordinates
-                    // float D = A * vUV.x + vUV.y + C;
-                    
-                    // // The color above the line is flipped if the angle is greater than 90 degrees
-                    // vec4 colorAbove = mix(colorB, colorA, isGreaterThanHalfPi);
-                    // vec4 colorBelow = mix(colorA, colorB, isGreaterThanHalfPi);
-
-                    // // If D>0, our coord is above the line
-                    // return mix(colorBelow, colorAbove, step(0.0, D));
+                    return mix(colorB, colorA, step(_mix_, vUV.y));
                 }
             `,
             },
@@ -101,7 +51,6 @@ export class WipeShaderBinding extends DisableableShaderBinding {
     private readonly _textureA: RuntimeData<ConnectionPointType.Texture>;
     private readonly _textureB: RuntimeData<ConnectionPointType.Texture>;
     private readonly _mix: RuntimeData<ConnectionPointType.Float>;
-    private readonly _angle: RuntimeData<ConnectionPointType.Float>;
 
     /**
      * Creates a new shader binding instance for the Wipe block.
@@ -109,20 +58,17 @@ export class WipeShaderBinding extends DisableableShaderBinding {
      * @param textureA - The first texture
      * @param textureB - The second texture
      * @param mix - The mix value between the two textures
-     * @param angle - The angle of the wipe
      */
     constructor(
         parentBlock: IDisableableBlock,
         textureA: RuntimeData<ConnectionPointType.Texture>,
         textureB: RuntimeData<ConnectionPointType.Texture>,
-        mix: RuntimeData<ConnectionPointType.Float>,
-        angle: RuntimeData<ConnectionPointType.Float>
+        mix: RuntimeData<ConnectionPointType.Float>
     ) {
         super(parentBlock);
         this._textureA = textureA;
         this._textureB = textureB;
         this._mix = mix;
-        this._angle = angle;
     }
 
     /**
@@ -134,7 +80,6 @@ export class WipeShaderBinding extends DisableableShaderBinding {
         effect.setTexture(this.getRemappedName("textureA"), this._textureA.value);
         effect.setTexture(this.getRemappedName("textureB"), this._textureB.value);
         effect.setFloat(this.getRemappedName("mix"), this._mix.value);
-        effect.setFloat(this.getRemappedName("angle"), this._angle.value);
     }
 }
 
@@ -168,11 +113,6 @@ export class WipeBlock extends DisableableShaderBlock {
     public readonly mix = this._registerInput("mix", ConnectionPointType.Float);
 
     /**
-     * The connection point containing the angle of the wipe (in degrees).
-     */
-    public readonly angle = this._registerInput("angle", ConnectionPointType.Float);
-
-    /**
      * The shader program (vertex and fragment code) to use to render the block
      */
     public static override ShaderCode = shaderProgram;
@@ -194,8 +134,7 @@ export class WipeBlock extends DisableableShaderBlock {
         const textureA = this._confirmRuntimeDataSupplied(this.textureA);
         const textureB = this._confirmRuntimeDataSupplied(this.textureB);
         const mix = this._confirmRuntimeDataSupplied(this.mix);
-        const angle = this._confirmRuntimeDataSupplied(this.angle);
 
-        return new WipeShaderBinding(this, textureA, textureB, mix, angle);
+        return new WipeShaderBinding(this, textureA, textureB, mix);
     }
 }
