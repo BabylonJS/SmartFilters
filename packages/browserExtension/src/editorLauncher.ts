5e7834dbaa5f21fb263fc9c9d25913c08092dba6
AmoebaChant <10319625+AmoebaChant@users.noreply.github.com>
2025-05-15 13:28:26 -0700

    tidy

--- packages/browserExtension/src/editorLauncher.ts
+++ packages/browserExtension/src/editorLauncher.ts
@@ -1,15 +1,16 @@
-import { SmartFilterEditor } from "@babylonjs/smart-filters-editor";
+/* eslint-disable no-console */
+import { SmartFilterEditorControl } from "@babylonjs/smart-filters-editor-control";
 
 const smartFilter = (window as any).currentSmartFilter;
 const thinEngine = (window as any).thinEngine;
 
 if (smartFilter) {
-    console.log("A SmartFilter was found in this document, launching the editor");
+    console.log("A SmartFilter was found in the page, launching the editor");
     // Display the editor
-    SmartFilterEditor.Show({
+    SmartFilterEditorControl.Show({
         engine: thinEngine,
         filter: smartFilter,
     });
 } else {
-    console.log("No SmartFilter was found in this document");
+    console.log("No SmartFilter was found in the page");
 }
